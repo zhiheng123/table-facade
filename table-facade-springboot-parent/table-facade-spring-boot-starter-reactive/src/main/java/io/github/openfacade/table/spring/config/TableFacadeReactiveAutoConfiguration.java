96f7fa8b445d943721392d8fa5fdd768763ca6d9
ZhangJian He <shoothzj@gmail.com>
2024-12-19 10:56:48 +0800

    feat: add reactive starter beans (#17)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- table-facade-springboot-parent/table-facade-spring-boot-starter-reactive/src/main/java/io/github/openfacade/table/spring/config/TableFacadeReactiveAutoConfiguration.java
+++ table-facade-springboot-parent/table-facade-spring-boot-starter-reactive/src/main/java/io/github/openfacade/table/spring/config/TableFacadeReactiveAutoConfiguration.java
@@ -16,11 +16,50 @@
 
 package io.github.openfacade.table.spring.config;
 
+import io.github.openfacade.table.api.DriverType;
+import io.github.openfacade.table.reactive.api.ReactiveTableManagement;
+import io.github.openfacade.table.reactive.api.ReactiveTableOperations;
 import io.github.openfacade.table.spring.core.TableFacadeProperties;
+import io.github.openfacade.table.spring.reactive.mysql.ReactiveMysqlTableManagement;
+import io.github.openfacade.table.spring.reactive.mysql.ReactiveMysqlTableOperations;
+import io.github.openfacade.table.spring.reactive.opengauss.ReactiveOpenGaussTableManagement;
+import io.github.openfacade.table.spring.reactive.opengauss.ReactiveOpenGaussTableOperations;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.r2dbc.core.DatabaseClient;
 
 @AutoConfiguration
 @EnableConfigurationProperties(TableFacadeProperties.class)
 public class TableFacadeReactiveAutoConfiguration {
+    private final TableFacadeProperties tableFacadeProperties;
+
+    private final DatabaseClient databaseClient;
+
+    public TableFacadeReactiveAutoConfiguration(TableFacadeProperties tableFacadeProperties,
+                                                DatabaseClient databaseClient) {
+        this.tableFacadeProperties = tableFacadeProperties;
+        this.databaseClient = databaseClient;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ReactiveTableOperations.class)
+    public ReactiveTableOperations reactiveTableOperations() {
+        if (tableFacadeProperties.getDriverType().equals(DriverType.openGauss)) {
+            return new ReactiveOpenGaussTableOperations(databaseClient);
+        } else {
+            return new ReactiveMysqlTableOperations(databaseClient);
+        }
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ReactiveTableManagement.class)
+    public ReactiveTableManagement reactiveTableManagement() {
+        if (tableFacadeProperties.getDriverType().equals(DriverType.openGauss)) {
+            return new ReactiveOpenGaussTableManagement(databaseClient);
+        } else {
+            return new ReactiveMysqlTableManagement(databaseClient);
+        }
+    }
 }
